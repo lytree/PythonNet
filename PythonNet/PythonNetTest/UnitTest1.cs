b309292261f74e260651cc87926544d5b95f5519
lytree <hi.yangzh@outlook.com>
2022-12-09 16:42:01 +0800

    init

---
+++ PythonNet/PythonNetTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+using PythonNetLib;
+using Xunit;
+
+namespace PythonNetTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            Console.WriteLine("------");
+            Test.Test1();
+            
+           
+        }
+    }
+}
---
+++ PythonNet/TestProject1/UnitTest1.cs
@@ -0,0 +1,14 @@
+using PythonNetLib;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Console.WriteLine("------");
+            Test.Test1();
+        }
+    }
+}
